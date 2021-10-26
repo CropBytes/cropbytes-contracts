d184a016ad9be24029f65968dde7d60ac9c45b94
Guru Vashist <guruv@cropbytes.com>
2021-10-26 02:30:47 +0000

    Removed floating Pragma

--- contracts/CropBytesVesting.sol
+++ contracts/CropBytesVesting.sol
@@ -1,7 +1,6 @@
-
 // contracts/TokenVesting.sol
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.0;
+pragma solidity 0.8.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/utils/SafeERC20.sol";
